032f6288bd71c8c18d7f7aedff88ec9c0ebfc68c
nbradf2 <nbradf2@gmail.com>
2017-10-28 18:51:28 -0500

    add test if input is not a number

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -22,10 +22,22 @@ describe('fizzBuzzer', function() {
       fizzBuzzer(input).should.equal('fizz');
     });
   });
-  // it('should return the input number if it is not divisible by 3, 5, or 15', function() {
-  //   const notDivisible = function(input) {
-      
-  //   }
-  // })
+  it('should return number if it is not divisible by 3, 5, or 15', function() {
+    [1, 2, 4, 14].forEach(function(input) {
+      fizzBuzzer(input).should.equal(input); 
+    });
+  });
+  it('should return error if input is not a number', function() {
+    const badInputs = [
+      false,
+      true,
+      'Ramona'
+    ];
+      badInputs.forEach(function(input) {
+        (function() {
+          fizzBuzzer(input[0])
+        }).should.throw(Error);
+      });
+    });
 });
 
