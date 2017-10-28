696f969400ebf18a716032e0660054c39ccae26f
nbradf2 <nbradf2@gmail.com>
2017-10-28 18:26:46 -0500

    add tests for expected fizzBuzzer answers

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -6,37 +6,26 @@ const fizzBuzzer = require('../fizzBuzzer');
 // if divisible by 5, expect 'buzz'
 // if divisible by 3, expect 'fizz'
 
-// Unit tests for 'fizzBuzzer' function
 describe('fizzBuzzer', function() {
-	//test the normal case
-	it('should determine if a number is divisible by 3, 5, or both', function() {
-		// range of normal inputs, inluding
-		// notable cases like negative answers
-		const normalCases = [
-			{num: -1, expected: -1}
-			{num: 1, expected: 1},
-			{num: 3, expected: 'fizz'},
-			{num: 5, expected: 'buzz'},
-			{num: 8, expected: 8},
-			{num: 15, expected: 'fizz-buzz'}
-		];
-		// for each input, 'fizzBuzzer' should produce
-		// the expected value
-		normalCases.forEach(function(input) {
-			const answer = fizzBuzzer(input.num);
-			answer.should.equal(input.expected);
-		});
-	});
-
-	it('should raise error if args are not numbers', function() {
-		// range of bad inputs where it is not a number
-		const badInputs = [
-			'one',
-			'three',
-			'five',
-			'eight',
-			'fifteen'
-		];
-	});
+  it('should determine if a number is divisible by 15', function() {
+    [15, 30, 45, 60].forEach(function(input) {
+      fizzBuzzer(input).should.equal('fizz-buzz');
+    });
+  });
+  it('should determine if a number is divisible by 5', function() {
+    [5, 10, 15, 20].forEach(function(input) {
+      fizzBuzzer(input).should.equal('buzz');
+    });
+  });
+  it('should determine if a number is divisible by 3', function() {
+    [3, 6, 9, 12].forEach(function(input) {
+      fizzBuzzer(input).should.equal('fizz');
+    });
+  });
+  // it('should return the input number if it is not divisible by 3, 5, or 15', function() {
+  //   const notDivisible = function(input) {
+      
+  //   }
+  // })
 });
 
